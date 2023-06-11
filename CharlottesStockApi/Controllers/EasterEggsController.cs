090d4b015fa6c2f645168a57926448dbc50bc96e
Philip Preisler <78653614+Philip3000@users.noreply.github.com>
2023-06-11 19:48:30 +0200

    Rest

---
+++ CharlottesStockApi/Controllers/EasterEggsController.cs
@@ -0,0 +1,72 @@
+﻿using CharlottesStockApi.Repository;
+using ChocolateLib;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CharlottesStockApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EasterEggsController : ControllerBase
+    {
+        private readonly EasterEggsRepository _repository;
+        public EasterEggsController(EasterEggsRepository repository)
+        {
+            _repository = repository;
+        }
+        // GET: api/<EasterEggsController>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet]
+        public ActionResult<IEnumerable<EasterEgg>> Get()
+        {
+            List<EasterEgg> easterEggs = _repository.Get();
+            if (easterEggs == null) return NotFound("No Easter Eggs exist");
+            return Ok(easterEggs);
+        }
+
+        // GET api/<EasterEggsController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("{productNo}")]
+        public ActionResult<EasterEgg> Get(int productNo)
+        {
+            EasterEgg easterEgg = _repository.GetByProductNo(productNo);
+            if (easterEgg == null)
+                return NotFound("No Easter Egg found with product number: " + productNo);
+            else
+                return Ok(easterEgg);
+        }
+        // GET api/<EasterEggsController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("lowStock/{stockLevel}")]
+        public ActionResult<List<EasterEgg>> GetLowStock(int stockLevel)
+        {
+            List<EasterEgg> easterEggStock = _repository.GetLowStock(stockLevel);
+            if (easterEggStock == null) return NotFound("No products with lower stock than" + stockLevel);
+            return Ok(easterEggStock);
+        }
+
+        // PUT api/<EasterEggsController>/5
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [HttpPut]
+        public ActionResult<EasterEgg> Put([FromBody] EasterEgg value)
+        {
+            try
+            {
+                EasterEgg updatedEasterEgg = _repository.Update(value);
+                return Ok(updatedEasterEgg);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ CharlottesStockApi/Repository/EasterEggsRepository.cs
@@ -0,0 +1,67 @@
+﻿using ChocolateLib;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CharlottesStockApi.Repository
+{
+    public class EasterEggsRepository
+    {
+        private readonly List<EasterEgg> Data;
+        public EasterEggsRepository() 
+        {
+            Data = new List<EasterEgg>
+            {
+                new EasterEgg{ProductNo = 8011, ChocolateType = "mørk", Price = 28, InStock = 5012},
+                new EasterEgg{ProductNo = 8012, ChocolateType = "mørk", Price = 32, InStock = 3420},
+                new EasterEgg{ProductNo = 8013, ChocolateType = "mørk", Price = 46, InStock = 1180},
+                new EasterEgg{ProductNo = 8022, ChocolateType = "lys", Price = 31, InStock = 2870},
+                new EasterEgg{ProductNo = 8023, ChocolateType = "lys", Price = 41, InStock = 1067},
+                new EasterEgg{ProductNo = 8032, ChocolateType = "hvid", Price = 34, InStock = 2017},
+            };
+        }
+        public List<EasterEgg> Get() 
+        { 
+            List<EasterEgg> data = Data;
+            return data;
+        }
+        public EasterEgg GetByProductNo(int productNo)
+        {
+            try
+            {
+                foreach (EasterEgg e in Data)
+                {
+                    if (e.ProductNo == productNo)
+                    {
+                        return e;
+                    }
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Something went wrong", ex);
+            }
+        }
+        public List<EasterEgg> GetLowStock(int stockLevel) 
+        {
+            List<EasterEgg> stockList = new List<EasterEgg>();
+            foreach (EasterEgg e in Data) 
+            {
+                if (e.InStock < stockLevel)
+                {
+                    stockList.Add(e);
+                }
+            }
+            return stockList;
+        }
+        public EasterEgg Update(EasterEgg update) 
+        {
+            EasterEgg easterEgg = GetByProductNo(update.ProductNo);
+            if (easterEgg == null) return null;
+            easterEgg.ProductNo = update.ProductNo;
+            easterEgg.ChocolateType = update.ChocolateType;
+            easterEgg.Price = update.Price;
+            easterEgg.InStock = update.InStock;
+            return easterEgg;
+        }
+    }
+}
